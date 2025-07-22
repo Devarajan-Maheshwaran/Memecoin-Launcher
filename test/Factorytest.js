44f07e221998184c5fb2a413eb7092c1b92cc93e
Devarajan Maheshwaran <devarajanmaheswaran.cse2024.citchennai.net>
2025-07-22 16:25:13 +0530

    Updated Factorytest.js

--- test/Factorytest.js
+++ test/Factorytest.js
@@ -4,14 +4,20 @@ const { ethers } = require("hardhat")
 
 describe ("Factory", function(){
 
-    it("should have a name", async function(){
+    async function Deployfactoryfixture() {
         // Fetch Contract
         const Factory = await ethers.getContractFactory("Factory")
         // Deploy Contract
-        const factory = await Factory.deploy();
+        const factory = await Factory.deploy()
+
+        return { factory }
+    }
+
+    it("should have a name", async function(){
+        const { factory } = await Deployfactoryfixture()
         // Check Name
-        const name = await factory.name();
-        expect(name).to.equal("Factory");
+        const name = await factory.name()
+        expect(name).to.equal("Factory")
         
 
     })
