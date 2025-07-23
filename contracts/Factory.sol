ce56c957403af7bd6021f596b8253816475401a1
Devarajan Maheshwaran <devarajanmaheswaran.cse2024.citchennai.net>
2025-07-23 16:24:35 +0530

    Updated Files

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,13 +1,33 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
+import { MyToken } from "./Tokens.sol";
+
 contract Factory{
     uint256 public immutable fee;
     address public owner; 
+    uint256 totalTokens;
+
+    address[] public tokens;
 
     constructor(uint256 _fee){
         fee = _fee;
         owner = msg.sender;
     }
     
+    event TokenCreated(address tokenAddress);
+
+    function createToken(string memory _name, string memory _symbol) external payable {
+        MyToken token = new MyToken(
+            msg.sender,
+            _name,
+            _symbol,
+            1_000_000 ether            
+        );
+        emit TokenCreated(address(token));
+
+        tokens.push(address(token));
+
+        totalTokens++;
+    }
 }
